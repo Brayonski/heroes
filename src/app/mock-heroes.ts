7319b3c0d4a1ea76080bac0b9edc1edda036e76c
root <root@localhost.localdomain>
2018-08-08 09:40:24 +0300

    added list

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,9 @@
+import { Hero } from './hero';
+
+export const HEROES:Hero[] = [
+    { id: 1, name:'mutai'},
+    { id: 2, name:'magneta'},
+    { id: 3, name:'Wayne'},
+    { id: 4, name:'Curter'},
+    { id: 5, name:'Blakey'}
+];
